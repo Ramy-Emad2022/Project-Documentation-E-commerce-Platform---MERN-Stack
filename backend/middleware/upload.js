4156470aaec7b93f23d7332840b74b4a5df78185
Ramy-Emad2022 <115583841+Ramy-Emad2022@users.noreply.github.com>
2025-07-08 09:38:15 -0700

    Add files via upload

---
+++ backend/middleware/upload.js
@@ -0,0 +1,15 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed!"), false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+module.exports = upload;
